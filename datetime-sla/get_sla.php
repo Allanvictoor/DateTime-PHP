080eededbbb5c1d882670d97aee1bf1b5d79b2f5
Allan VBatista <allanbatista2010@gmail.com>
2020-05-20 11:42:55 -0300

    melhorias na missao sla php

--- datetime-sla/get_sla.php
+++ datetime-sla/get_sla.php
@@ -9,13 +9,18 @@ function is_horautil(\DateTime $data)
     $inicioDoSegundoTurno = clone $horautil->setTime(13, 0, 0);
     $fimDoSegundoTurno = clone $horautil->setTime(18, 0, 0);
 
-    if (($inicioDoPrimeiroTurno < $horautil && $horautil < $fimDoPrimeiroTurno) || ($inicioDoSegundoTurno < $horautil && $horautil < $fimDoSegundoTurno)) {
+    if (($inicioDoPrimeiroTurno < $data && $data < $fimDoPrimeiroTurno) || ($inicioDoSegundoTurno < $data && $data < $fimDoSegundoTurno)) {
         return true;
     }
-   
+ 
+     else  {
+     return false;
+     }   
     
     
 }
+
+
 
 
 
@@ -31,27 +36,20 @@ function get_sla(\DateTime $inicio, $sla)
         throw new \Exception('Informe um valor inteiro para SLA');
     }
     $sla = (int) $sla;
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    
-=======
     // ao converter pra minutos, podemos facilitar as coisas
     $slaEmMinutos = $sla * 60;
->>>>>>> upstream/master
-
-=======
-    $slaEmMinutos = $sla * 60;
-    
->>>>>>> 79a8c83f1f0f23b7b4794a323fcf3929e3e0df80
     $prazo = new DateTime($inicio->format('Y-m-d H:i'));
     $i = 0;
     while ($i < $slaEmMinutos) {
-        // - adicione 1 minuto em $prazo
-
+        
         if (is_horautil($prazo)) {
             $i++;
+            
         }
+       
+        $minutos = new DateInterval("PT{$i}M");
+        $prazo->add($minutos);
+    
     
     }
 
