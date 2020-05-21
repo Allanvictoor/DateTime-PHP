e14f7259282de853be5feabc56026f41b3c076ff
allanbatista-vwi <allanbatista@vivaweb.net>
2020-05-21 16:06:15 -0300

    deixando missao recorrencia dinamico

--- datatime-recorrencia/get_recorrencia.php
+++ datatime-recorrencia/get_recorrencia.php
@@ -1,49 +1,38 @@
 <?php
 
+
 function get_recorrencia(\DateTime $inicio, $options)
 {
-    
 
- 
-    $dataInicial = new DateTime('2020-01-01'); //Data de criacao
-    $dataDeRepeticao = new DateTime('2020-02-01'); //Data a ser repetida
-    $intervaloDiario = new DateInterval('P1D');
-    $intervaloSemanal = new DateInterval('P7D');
-    $intervaloMensal = new DateInterval('P1M');
-    $intervaloAnual = new DateInterval('P1Y');
-    $dataFinal = new DateTime('2020-09-02'); //Data de encerramento
-    $quantidade = 3;
-    
-    $periodo = new DatePeriod($dataDeRepeticao, $intervaloMensal, $dataFinal);
-    $periodoQuant = new DatePeriod($dataDeRepeticao, $intervaloSemanal, $quantidade);
 
-    $count = [];
+
     $datas = [];
-    foreach ($periodo as $data) {
-        if ($datas < $dataFinal) {
-            $datas[] = $data->format('d-m-Y');
-        
+    if($options == 'diaria') {
+
+        if($options->termina_em != null) {
+
+            while($inicio <= $options->termina_em) {
+
+                if($options->por_mes != null) {
+
+                }
+                if($options->por_dia != null) {
+
+                }
+                if($options->por_dia_mes != null) {
+
+                }
+
+                $inicio = $inicio->add(new DateInterval('P1D'));
+            }
+
+            return $datas;
+
+
+            exit();
 
         }
-        $datas++;
-        var_dump ($datas). "<br>";
-        
-        
-        
+
     }
 
-    foreach ($periodoQuant as $data2) {
-        if ($count <= $periodoQuant) {
-            $count[] = $data2->format('d-m-Y') . "<br>";
-        }
-        $count++;
-    }
-
-   
-    
-    return $data;
-    
 }
-
-  
-
