87bb53ac8f249178700e8b6e44abf4e602fc49e0
allanbatista-vwi <allanbatista@vivaweb.net>
2020-06-01 17:27:38 -0300

    passou nos dois testes

--- datatime-recorrencia/get_recorrencia.php
+++ datatime-recorrencia/get_recorrencia.php
@@ -9,75 +9,26 @@ function get_recorrencia(\DateTime $inicio, $options)
     $semanal = new DateInterval('P1W');
     $mensal = new DateInterval('P1M');
     $anual = new DateInterval('P1Y');
-    $dates_datafinal = [];
-    $dates_quantidade = [];
+    $dates = [];
 
 
 
-        if ($options['frequencia'] == "diario") {
-
-            $periodoDiario_date_final = new DatePeriod($data_inicio, $diario, $data_final);
-
-            foreach ($periodoDiario_date_final as $data) {
-                $dates_datafinal[] = new $data;
 
 
-            }
+        if ($options['frequencia'] == "diaria" && $options['quantidade'] == null) {
 
-            if (end($dates_datafinal) <= $data_final ) {
-                array_push($dates_datafinal, $data_final);
-            }
-            print_r($dates_datafinal);
+            $periodo = new DatePeriod($data_inicio, $diario, $data_final);
 
+            foreach ($periodo as $data) {
 
-        }
-
-
-        if ($options['frequencia'] == "semanal") {
-
-            $periodoSemanal_date_final = new DatePeriod($data_inicio, $semanal, $data_final);
-
-            foreach ($periodoSemanal_date_final as $data) {
-                $dates_datafinal[] =$data;
-            }
-            if (end($dates_datafinal) <= $data_final ) {
-                array_push($dates_datafinal, $data_final);
-            }
-            print_r($dates_datafinal);
-
-        }
-
-
-        if ($options['frequencia'] == "mensal") {
-
-            $periodoMesal_date_final = new DatePeriod($data_inicio, $mensal, $data_final);
-
-            foreach ($periodoMesal_date_final as $data) {
-
-                $dates_datafinal[] = $data;
+                $dates[] = $data;
 
 
             }
-            if (end($dates_datafinal) <= $data_final ) {
-                array_push($dates_datafinal, $data_final);
+            if (end($dates ) <= $data_final ) {
+                array_push($dates , $data_final);
             }
-            print_r($dates_datafinal);
-
-
-        }
-
-
-        if ($options['frequencia'] == "anual") {
-
-            $periodoAnual_date_final = new DatePeriod($data_inicio, $anual, $data_final);
-
-            foreach ($periodoAnual_date_final as $data) {
-                $dates_datafinal[] = $data;
-            }
-            if (end($dates_datafinal) <= $data_final ) {
-                array_push($dates_datafinal, $data_final);
-            }
-            print_r($dates_datafinal);
+            print_r($dates);
 
 
         }
@@ -85,14 +36,58 @@ function get_recorrencia(\DateTime $inicio, $options)
 
 
 
+        if ($options['frequencia'] == "semanal" && $options['quantidade'] == null) {
+
+            $periodo = new DatePeriod($data_inicio, $semanal, $data_final);
+
+            foreach ($periodo as $data) {
+
+                $dates[] = $data;
 
 
-        if ($options['frequencia'] == 'semanal') {
-            $quantidade_semanal = new DatePeriod($data_inicio, $semanal, $options['quantidade']);
-            foreach ($quantidade_semanal as $data) {
-                $dates_quantidade[] = $data;
             }
-            print_r($dates_quantidade);
+            if (end($dates ) <= $data_final ) {
+                array_push($dates , $data_final);
+            }
+            print_r($dates);
+
+
+        }
+
+        if ($options['frequencia'] == "mensal" && $options['quantidade'] == null) {
+
+            $periodo = new DatePeriod($data_inicio, $mensal, $data_final);
+
+            foreach ($periodo as $data) {
+
+                $dates[] = $data;
+
+
+            }
+            if (end($dates ) <= $data_final ) {
+                array_push($dates , $data_final);
+            }
+            print_r($dates);
+
+
+        }
+
+        if ($options['frequencia'] == "anual" && $options['quantidade'] == null) {
+
+            $periodo = new DatePeriod($data_inicio, $anual, $data_final);
+
+            foreach ($periodo as $data) {
+
+                $dates[] = $data;
+
+
+            }
+            if (end($dates ) <= $data_final ) {
+                array_push($dates , $data_final);
+            }
+            print_r($dates);
+
+
         }
 
 
@@ -101,8 +96,59 @@ function get_recorrencia(\DateTime $inicio, $options)
 
 
 
-        return $dates_datafinal;
-        return $dates_quantidade;
+
+
+        if ($options['frequencia'] == 'diario' && $options['termina_em'] == null) {
+            $quantidade = $options['quantidade'];
+            $quantidade = $quantidade - 1;
+            $periodo = new DatePeriod($data_inicio, $diario, $quantidade);
+            foreach ($periodo as $data) {
+                $dates[]  = $data;
+            }
+            print_r($dates);
+        }
+
+        if ($options['frequencia'] == 'semanal' && $options['termina_em'] == null) {
+            $quantidade = $options['quantidade'];
+            $quantidade = $quantidade - 1;
+            $periodo = new DatePeriod($data_inicio, $semanal, $quantidade);
+            foreach ($periodo as $data) {
+                $dates[]  = $data;
+            }
+            print_r($dates);
+        }
+
+        if ($options['frequencia'] == 'mensal' && $options['termina_em'] == null) {
+            $quantidade = $options['quantidade'];
+            $quantidade = $quantidade - 1;
+            $periodo = new DatePeriod($data_inicio, $mensal, $quantidade);
+            foreach ($periodo as $data) {
+                $dates[]  = $data;
+            }
+            print_r($dates);
+        }
+
+        if ($options['frequencia'] == 'anual' && $options['termina_em'] == null) {
+            $quantidade = $options['quantidade'];
+            $quantidade = $quantidade - 1;
+            $periodo = new DatePeriod($data_inicio, $anual, $quantidade);
+            foreach ($periodo as $data) {
+                $dates[]  = $data;
+            }
+            print_r($dates);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        return $dates ;
 
 
 
