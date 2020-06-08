5ae48c2ea8ee0eef3138e5c626e6271bbe4af8fa
allanbatista-vwi <allanbatista@vivaweb.net>
2020-06-08 16:23:08 -0300

    corigindo identacao, if repetidos, espacamentos, variaveis

--- datatime-recorrencia/get_recorrencia.php
+++ datatime-recorrencia/get_recorrencia.php
@@ -3,143 +3,32 @@
 
 function get_recorrencia(\DateTime $inicio, $options)
 {
-    $data_inicio = clone $inicio;
-    $data_inicio_recorrencia = clone $inicio;
-    $por_dia_mes = $options['por_dia_mes'];
-    $por_dia = $options['por_dia'];
-    $data_inicio->setDate('2020','02', $por_dia_mes);
-    $data_inicio_recorrencia->setDate('2020','02', $por_dia);
-    $data_final = new DateTime($options["termina_em"]);
+    $dataInicioRecorrencia = clone $inicio;
+    $inicio->setDate($inicio, $inicio, $options['por_dia_mes']);
+
     $diario = new DateInterval(' P1D');
     $semanal = new DateInterval('P1W');
     $mensal = new DateInterval('P1M');
     $anual = new DateInterval('P1Y');
+
     $dates = [];
 
-
-
-
-
-        if ($options['frequencia'] == "diaria" && $options['quantidade'] == null) {
-
-            $periodo = new DatePeriod($data_inicio, $diario, $data_final);
-
-            foreach ($periodo as $data) {
-
-                $dates[] = $data;
-
-
-            }
-            if (end($dates ) <= $data_final ) {
-                array_push($dates , $data_final);
-            }
-            print_r($dates);
-
-
+    if ($options['termina_em'] == true) {
+        if ($options['frequencia'] == 'diaria') {
+            $periodo = new DatePeriod($inicio, $diario, $options['termina_em']);
         }
-
-
-
-
-        if ($options['frequencia'] == "semanal" && $options['quantidade'] == null) {
-
-            $periodo = new DatePeriod($data_inicio, $semanal, $data_final);
-
-            foreach ($periodo as $data) {
-
-                $dates[] = $data;
-
-
-            }
-            if (end($dates ) <= $data_final ) {
-                array_push($dates , $data_final);
-            }
-            print_r($dates);
-
-
+        if ($options['frequencia'] == 'semanal') {
+            $periodo = new DatePeriod($inicio, $semanal, $options['termina_em']);
         }
-
-        if ($options['frequencia'] == "mensal" && $options['quantidade'] == null) {
-
-            $periodo = new DatePeriod($data_inicio, $mensal, $data_final);
-
-            foreach ($periodo as $data) {
-
-                $dates[] = $data;
-
-
-            }
-            if (end($dates ) <= $data_final ) {
-                array_push($dates , $data_final);
-            }
-            print_r($dates);
-
-
+        if ($options['frequencia'] == 'mensal') {
+            $periodo = new  DatePeriod($inicio, $mensal, $options['termina_em']);
         }
-
-        if ($options['frequencia'] == "anual" && $options['quantidade'] == null) {
-
-            $periodo = new DatePeriod($data_inicio, $anual, $data_final);
-
-            foreach ($periodo as $data) {
-
-                $dates[] = $data;
-
-
-            }
-            if (end($dates ) <= $data_final ) {
-                array_push($dates , $data_final);
-            }
-            print_r($dates);
-
-
+        if ($options['frequencia'] == 'anual') {
+            $periodo = new DatePeriod($inicio, $anual, $options['termina_em']);
         }
-
-
-        if ($options['frequencia'] == 'diario' && $options['termina_em'] == null) {
-            $quantidade = $options['quantidade'];
-            $quantidade = $quantidade - 1;
-            $periodo = new DatePeriod($data_inicio_recorrencia, $diario, $quantidade);
-            foreach ($periodo as $data) {
-                $dates[]  = $data;
-            }
-            print_r($dates);
+        foreach ($periodo as $data) {
+            $dates[] = $data;
         }
-
-        if ($options['frequencia'] == 'semanal' && $options['termina_em'] == null) {
-            $quantidade = $options['quantidade'];
-            $quantidade = $quantidade - 1;
-            $periodo = new DatePeriod($data_inicio_recorrencia, $semanal, $quantidade);
-            foreach ($periodo as $data) {
-                $dates[]  = $data;
-            }
-            print_r($dates);
-        }
-
-        if ($options['frequencia'] == 'mensal' && $options['termina_em'] == null) {
-            $quantidade = $options['quantidade'];
-            $quantidade = $quantidade - 1;
-            $periodo = new DatePeriod($data_inicio_recorrencia, $mensal, $quantidade);
-            foreach ($periodo as $data) {
-                $dates[]  = $data;
-            }
-            print_r($dates);
-        }
-
-        if ($options['frequencia'] == 'anual' && $options['termina_em'] == null) {
-            $quantidade = $options['quantidade'];
-            $quantidade = $quantidade - 1;
-            $periodo = new DatePeriod($data_inicio_recorrencia, $anual, $quantidade);
-            foreach ($periodo as $data) {
-                $dates[]  = $data;
-            }
-            print_r($dates);
-        }
-
-        return $dates ;
-
-
-
-
+    }
+    return $dates;
 }
-
