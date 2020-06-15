19d8f2e3d64fa717bec07a1399cc62b9a523ad79
allanbatista-vwi <allanbatista@vivaweb.net>
2020-06-15 17:05:36 -0300

    modificando teste visual

--- datetime-sla-2-feriados/test.php
+++ datetime-sla-2-feriados/test.php
@@ -1,12 +1,12 @@
 <?php
 
 include 'get_sla.php';
-$dataFeriado = new DateTime('2020-06-10 17:00:00');
+$data = new DateTime('2020-06-10 17:00:00');
 
-$t1 = get_sla($dataFeriado, 2);
+$t1 = get_sla($data, 2);
 echo "Espera-se 2020-06-12 09:00" . PHP_EOL;
 var_dump($t1);
 
-$t2 = get_sla($dataFeriado, 10);
+$t2 = get_sla($data, 10);
 echo "Espera-se 2020-06-15 08:15" . PHP_EOL;
 var_dump($t2);
